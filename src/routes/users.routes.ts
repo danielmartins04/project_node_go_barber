1be53436da7906445472560b39d005457875d59d
daniel martins <freitasdaniel39@gmail.com>
2021-03-23 20:23:49 -0300

    password encryption

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,17 +4,25 @@ import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
 
+interface UserDto {
+  name: string;
+  email: string;
+  password?: string;
+}
+
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
 
     const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
+    const user: UserDto = await createUser.execute({
       name,
       email,
       password,
     });
+
+    delete user.password;
 
     return response.json(user);
   } catch (err) {
