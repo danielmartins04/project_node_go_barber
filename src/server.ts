b4f404859943f77230254e4d489b97344d78e4d8
daniel martins <freitasdaniel39@gmail.com>
2021-03-25 21:26:13 -0300

    defining static route

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,14 @@ import 'reflect-metadata';
 
 import express, { json } from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
